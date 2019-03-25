de80c296109c5444dbf713fe856d3716253951d9
beatrizguergolet <48692982+beatrizguergolet@users.noreply.github.com>
2019-03-24 23:36:42 -0300

    Update mandelbrot.jl

--- mandelbrot.jl
+++ mandelbrot.jl
@@ -1,36 +1,35 @@
-# add Images
-# add ImageMagick
-
 using Images
-
-# z^2 = x^2 + 2xyi - y^2 = (x^2 - y^2) + (2xy) i
-function iteracao_mandelbrot(cx, cy; max_iter = 40)
-  iter = 0
-
-  # AQUI
-
-  if x^2 + y^2 < 4
+function interacao_mandelbrot(cx, cy; max_inter = 60)
+  x = 0
+  y = 0
+  for inter = 0:60
+      p = x
+      q = y
+      x = p ^ 2 - q ^ 2 + cx
+      y = 2p*q + cy
+  end
+  if x ^ 2 + y ^ 2  <  4
     return false, 0
   else
-    return true, iter / max_iter
+    return true, 1
   end
 end
 
 function mandelbrot(N)
-  x = range(-2.5, 1.5, length=N)
-  y = range(-2.0, 2.0, length=N)
+  x =  intervalo( - 2,5 , 1,5 , length=N)
+  y =  intervalo( - 2,0 , 2,0 , length=N)
 
   imagem = zeros(N, N)
-  for i = 1:N
-    for j = 1:N
-      estavel, ρ = iteracao_mandelbrot(x[i], y[j])
-      if estavel
-        imagem[i,j] = ρ
+  for i = 1 : N
+    for j = 1 : N
+      explodiu, ρ = interacao_mandelbrot(x[i], y[j])
+      if explodiu
+        imagem[i, j] = ρ
       end
     end
   end
 
-  save("mandelbrot.jpg", imagem')
+  save("mandelbrot.jpg", imagem)
 end
 
-mandelbrot(1000)
+mandelbrot(2000)
